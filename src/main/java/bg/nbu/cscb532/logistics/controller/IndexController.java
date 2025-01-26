b4a65760defe00a2fc71ee847722d40a658a80ba
Yasen Georgiev <yasen@tavex.eu>
2025-01-26 23:02:18 +0200

    Add example with `@PreAuthorize

--- src/main/java/bg/nbu/cscb532/logistics/controller/IndexController.java
+++ src/main/java/bg/nbu/cscb532/logistics/controller/IndexController.java
@@ -1,15 +1,28 @@
 package bg.nbu.cscb532.logistics.controller;
 
+import bg.nbu.cscb532.logistics.data.enumeration.Authority;
+import bg.nbu.cscb532.logistics.data.repository.ShippingRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
+    private final ShippingRepository shippingRepository;
+
     @GetMapping
     public String index(Model model) {
         model.addAttribute("title", "Home");
 
+        return "index";
+    }
+
+    @GetMapping("/test")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String test() {
         return "index";
     }
 }
