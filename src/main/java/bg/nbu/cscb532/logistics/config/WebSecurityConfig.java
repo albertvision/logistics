e0a600959c8fd5fb907032921bc9b19cf23a5058
Yasen Georgiev <yasen@tavex.eu>
2025-02-03 11:09:44 +0200

    Fix home page.

--- src/main/java/bg/nbu/cscb532/logistics/config/WebSecurityConfig.java
+++ src/main/java/bg/nbu/cscb532/logistics/config/WebSecurityConfig.java
@@ -1,13 +1,17 @@
 package bg.nbu.cscb532.logistics.config;
 
+import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.HashMap;
 
@@ -16,6 +20,26 @@ import java.util.HashMap;
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authz ->
+                authz
+                    .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.ERROR).permitAll()
+                    .requestMatchers(
+                        "/",
+                        "/assets/**",
+                        "/css/**",
+                        "/js/**",
+                        "/static/**"
+                    ).permitAll()
+                    .anyRequest().authenticated()
+            )
+            .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+
     @Bean
     PasswordEncoder passwordEncoder() {
         String defaultEncoderId = "bcrypt";
--- src/main/java/bg/nbu/cscb532/logistics/controller/IndexController.java
+++ src/main/java/bg/nbu/cscb532/logistics/controller/IndexController.java
@@ -1,7 +1,7 @@
 package bg.nbu.cscb532.logistics.controller;
 
-import bg.nbu.cscb532.logistics.data.enumeration.Authority;
 import bg.nbu.cscb532.logistics.data.repository.ShippingRepository;
+import bg.nbu.cscb532.logistics.service.CompanyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -12,10 +12,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 public class IndexController {
     private final ShippingRepository shippingRepository;
+    private final CompanyService companyService;
 
     @GetMapping
     public String index(Model model) {
         model.addAttribute("title", "Home");
+        model.addAttribute("company", companyService.find());
 
         return "index";
     }
